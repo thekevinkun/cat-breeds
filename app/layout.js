52c563beb124408e88f0be0aa85fa4f782f75cdd
thekevinkun <developer.kevinkun@gmail.com>
2025-05-21 20:48:05 +0800

    Added favicon

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,31 @@
 import "@/styles/globals.css";
 
+export const viewport = {
+  theme_color: "#ababab",
+};
+
 export const metadata = {
   title: "Cat Breeds",
   description: "Discover cat breeds around the world.",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+    other: [
+      {
+        rel: "icon",
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        rel: "icon",
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+  manifest: "/manifest.webmanifest",
 };
 
 export default function RootLayout({ children }) {
