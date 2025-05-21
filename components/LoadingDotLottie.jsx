b068530efc3fc1db27d4620f8d860ab5e3459f04
thekevinkun <developer.kevinkun@gmail.com>
2025-05-21 21:38:52 +0800

    Fix document error on build

--- components/LoadingDotLottie.jsx
+++ components/LoadingDotLottie.jsx
@@ -1,27 +1,30 @@
-import React from "react";
-import Lottie from "lottie-react";
+"use client"
+
+import dynamic from "next/dynamic";
 import loadingAnimation from "@/public/animations/loading-lottie.json";
+
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 const LoadingDotLottie = ({ isReady }) => {
   return (
     <div 
         style={{
-            position: "absolute",
-            inset: "0",
-            zIndex: "9999",
-            background: "white",
-            width: "100%",
-            height: "100svh",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            opacity: isReady ? 0 : 1,
-            pointerEvents: isReady ? "none" : "all",
-            transition: "opacity 0.5s ease",
+          position: "absolute",
+          inset: "0",
+          zIndex: "9999",
+          background: "white",
+          width: "100%",
+          height: "100svh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          opacity: isReady ? 0 : 1,
+          pointerEvents: isReady ? "none" : "all",
+          transition: "opacity 0.5s ease",
         }}
     >
         <div style={{width: "125px", height: "125px"}}>
-            <Lottie animationData={loadingAnimation} loop autoplay />
+          <Lottie animationData={loadingAnimation} loop autoplay />
         </div>
     </div>
   );
